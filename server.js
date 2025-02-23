847962343e0ea88cc450475a4aeaa79f92922570
Sampo Pelto <pelto.sampo@gmail.com>
2025-02-23 14:52:54 +0200

    refactor

--- server.js
+++ server.js
@@ -28,8 +28,7 @@ function createIframeCard(url, comment) {
           <iframe src="${url}" width="100%" height="600" style="border: none; pointer-events: none;"> </iframe>
         </div>
       </div>
-    </div>
-  `;
+    </div>`;
 }
 
 // Create footer with repo url
@@ -38,51 +37,43 @@ function createFooter() {
     <footer class="text-center mt-5">
       <hr />
       <p>
-        <a href="https://github.com/sampozki/tupsulabussipys-kki" target="_blank" rel="noopener noreferrer">
-          Repositorio
-        </a>
+        <a href="https://github.com/sampozki/tupsulabussipys-kki" target="_blank" rel="noopener noreferrer">Repositorio</a>
       </p>
-    </footer>
-  `;
+    </footer>`;
 }
 
 const app = express();
-const PORT = 3000;
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send(`
-  <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Tupsulan bussipysäkit</title>
+  res.send(
+`<!DOCTYPE html>
+<html lang="fi">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Tupsulan bussipysäkit</title>
 
-        <!-- local version of https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/vapor/bootstrap.min.css --> 
-        <link rel="stylesheet" href="/bootstrap.min.css">
-        
-        <link rel="icon" href="/favicon.ico">
-      </head>
-      <body>
-        <div class="container py-4">
-          <!-- Heading card -->
-          ${createHeadingCard()}
+    <!-- local version of https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/vapor/bootstrap.min.css --> 
+    <link rel="stylesheet" href="/bootstrap.min.css">
+    
+    <link rel="icon" href="/favicon.ico">
+  </head>
+  <body>
+    <div class="container py-4">
+      ${createHeadingCard()}
 
-          <!-- Second row with two iframes -->
-          <div class="row g-4">
-            ${createIframeCard('https://tremonitori.digitransit.fi/view?cont=OegRrZntBRck8KebSxJC8w==', 'Bus stop to keskusta')}
-            ${createIframeCard('https://tremonitori.digitransit.fi/view?cont=R0cybxFsZRC-uy2zCdbUdg==', 'Bus stop to Hervanta')}
-          </div>
-
-            ${createFooter()}
-
-        </div>
-      </body>
-    </html>
-  `);
+      <div class="row g-4">
+        ${createIframeCard('https://tremonitori.digitransit.fi/view?cont=OegRrZntBRck8KebSxJC8w==', 'Bus stop to keskusta')}
+        ${createIframeCard('https://tremonitori.digitransit.fi/view?cont=R0cybxFsZRC-uy2zCdbUdg==', 'Bus stop to Hervanta')}
+      </div>
+        ${createFooter()}
+    </div>
+  </body>
+</html>`);
 });
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
