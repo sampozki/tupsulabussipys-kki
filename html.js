63e4ab98b0e69445f442294a27d26d673ac3a751
Sampo Pelto <pelto.sampo@gmail.com>
2025-02-23 15:56:13 +0200

    refactoring

--- html.js
+++ html.js
@@ -1,9 +1,11 @@
-module.exports = { htmlPage };
+module.exports = {
+  htmlPage
+};
 
 // Create heading card
 function createHeadingCard() {
-    return `
-      <div class="card">
+  return (
+    `<div class="card">
         <div class="card-body">
           <div class="d-flex flex-column flex-sm-row justify-content-center">
             <img src="/tupsbus.png" alt="Logo" class="img-fluid mb-2 mb-sm-0 me-sm-2 ratio ratio-1x1" style="width: 80px;"/>
@@ -13,37 +15,36 @@ function createHeadingCard() {
             </div>
           </div>
         </div>
-      </div>
-    `;
-  }
-  
-  // Create bus timetable iframe
-  function createIframeCard(url, comment) {
-    return `
-      <div class="col-md-6">
+      </div>`);
+}
+
+// Create bus timetable iframe
+function createIframeCard(url, comment) {
+  return (
+    `<div class="col-md-6">
         <div class="card h-100">
           <div class="card-body p-0">
             <!-- ${comment} -->
             <iframe src="${url}" width="100%" height="600" style="border: none; pointer-events: none;"> </iframe>
           </div>
         </div>
-      </div>`;
-  }
-  
-  // Create footer with repo url
-  function createFooter() {
-    return `
-      <footer class="text-center mt-5">
-        <hr />
-        <p>
-          <a href="https://github.com/sampozki/tupsulabussipys-kki" target="_blank" rel="noopener noreferrer">Repositorio</a>
-        </p>
-      </footer>`;
-  }
-  
+      </div>`);
+}
+
+// Create footer with repo url
+function createFooter() {
+  return (
+    `<footer class="text-center mt-5">
+            <hr />
+            <p>
+              <a href="https://github.com/sampozki/tupsulabussipys-kki" target="_blank" rel="noopener noreferrer">Repositorio</a>
+            </p>
+      </footer>`);
+}
+
 function htmlPage() {
-    return(
-`<!DOCTYPE html>
+  return (
+    `<!DOCTYPE html>
   <html lang="fi">
     <head>
       <meta charset="utf-8" />
@@ -58,7 +59,6 @@ function htmlPage() {
     <body>
       <div class="container py-4">
         ${createHeadingCard()}
-  
         <div class="row g-4">
           ${createIframeCard('https://tremonitori.digitransit.fi/view?cont=OegRrZntBRck8KebSxJC8w==', 'Bus stop to keskusta')}
           ${createIframeCard('https://tremonitori.digitransit.fi/view?cont=R0cybxFsZRC-uy2zCdbUdg==', 'Bus stop to Hervanta')}
@@ -66,8 +66,5 @@ function htmlPage() {
           ${createFooter()}
       </div>
     </body>
-  </html>
-  `);
+  </html>`);
 }
-
-
--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 
 // Log events 
 app.use((req, res, next) => {
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;  
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   console.log(`[${new Date().toISOString()}] ${ip} requested: ${req.method} ${req.url}`);
   next();
 });
 
-// Serve public folder's content
+// Serve content from public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve html.htmlPage()
@@ -19,7 +19,6 @@ app.get('/', (req, res) => {
   res.send(html.htmlPage());
 });
 
-// Listen to port 3000
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
