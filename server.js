9ec0745610af3872cbdb15f38535804bce44d3c1
Sampo Pelto <pelto.sampo@gmail.com>
2025-02-23 15:38:36 +0200

    Enable logging

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const html = require("./html.js")
-
 const express = require('express');
 const path = require('path');
 
 const app = express();
+
+// Log events 
+app.use((req, res, next) => {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;  
+  console.log(`[${new Date().toISOString()}] ${ip} requested: ${req.method} ${req.url}`);
+  next();
+});
+
+// Serve public folder's content
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve html.htmlPage()
 app.get('/', (req, res) => {
   res.send(html.htmlPage());
 });
 
+// Listen to port 3000
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
